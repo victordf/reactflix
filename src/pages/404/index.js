94ff55c8b604347650cd31df53ca2245be1a6fb7
Victor Martins Machado <victormachado90@gmail.com>
2020-07-29 00:45:35 -0300

    Denhando as imagens do flappy bird

--- src/pages/404/index.js
+++ src/pages/404/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
 
 import sprites from './assets/img/sprites.png'
@@ -7,22 +7,105 @@ const Canvas = styled.canvas`
   border: 1px solid #000;
   display: block;
   margin: 0 auto;
-  width: 320px;
-  height: 480px;
 `
 
 export default function Error404() {
+  let flappyBird = null
+  let chao = null
+  let planoDeFundo = null
   const img = new Image()
   img.src = sprites
   let ctx = null
 
   function loadContext(c) {
-    ctx = c.getContext('2d')
+    if (c) {
+      ctx = c.getContext('2d')
+      flappyBird = {
+        spriteX: 0,
+        spriteY:0,
+        largura: 33,
+        altura: 24,
+        x: 10,
+        y: 50,
+        desenha() {
+          ctx.drawImage(
+            img,
+            flappyBird.spriteX, flappyBird.spriteY, //SpriteX, SpriteY
+            flappyBird.largura, flappyBird.altura, // Tamanho do recorte na sprite x e y
+            flappyBird.x, flappyBird.y,
+            flappyBird.largura, flappyBird.altura
+          )
+        }
+      }
+
+      chao = {
+        spriteX: 0,
+        spriteY: 610,
+        largura: 224,
+        altura: 112,
+        x: 0,
+        y: c.height - 112,
+        desenha() {
+          ctx.drawImage(
+            img,
+            chao.spriteX, chao.spriteY,
+            chao.largura, chao.altura,
+            chao.x, chao.y,
+            chao.largura, chao.altura
+          )
+          ctx.drawImage(
+            img,
+            chao.spriteX, chao.spriteY,
+            chao.largura, chao.altura,
+            (chao.x + chao.largura), chao.y,
+            chao.largura, chao.altura
+          )
+        }
+      }
+
+      planoDeFundo = {
+        spriteX: 390,
+        spriteY: 0,
+        largura: 275,
+        altura: 204,
+        x: 0,
+        y: c.height - 204,
+        desenha() {
+          ctx.fillStyle = '#70c5ce'
+          ctx.fillRect(0,0, c.width, c.height)
+          ctx.drawImage(
+            img,
+            planoDeFundo.spriteX, planoDeFundo.spriteY, //SpriteX, SpriteY
+            planoDeFundo.largura, planoDeFundo.altura, // Tamanho do recorte na sprite x e y
+            planoDeFundo.x, planoDeFundo.y,
+            planoDeFundo.largura, planoDeFundo.altura
+          )
+          ctx.drawImage(
+            img,
+            planoDeFundo.spriteX, planoDeFundo.spriteY, //SpriteX, SpriteY
+            planoDeFundo.largura, planoDeFundo.altura, // Tamanho do recorte na sprite x e y
+            (planoDeFundo.x + planoDeFundo.largura), planoDeFundo.y,
+            planoDeFundo.largura, planoDeFundo.altura
+          )
+        }
+      }
+
+      function loop() {
+        planoDeFundo.desenha()
+        chao.desenha()
+        flappyBird.desenha()
+
+        // flappyBird.y = flappyBird.y+1
+
+        requestAnimationFrame(loop)
+      }
+      loop()
+    }
   }
 
   return (
     <>
-      <Canvas id="game-canvas" ref={loadContext} />
+      <Canvas id="game-canvas" width="320" height="480" ref={loadContext} />
     </>
   )
 }
