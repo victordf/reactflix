3855f2459c05d1ddb1b2317b549dfc591918c6f4
Victor Martins Machado <victormachado90@gmail.com>
2020-07-29 06:20:15 -0300

    Criação da tela de início e das transições de telas

--- src/pages/404/index.js
+++ src/pages/404/index.js
@@ -13,6 +13,7 @@ export default function Error404() {
   let flappyBird = null
   let chao = null
   let planoDeFundo = null
+  let mensagemGetReady = null
   const img = new Image()
   img.src = sprites
   let ctx = null
@@ -96,15 +97,72 @@ export default function Error404() {
         }
       }
 
+      mensagemGetReady = {
+        sX: 134,
+        sY: 0,
+        w: 174,
+        h: 152,
+        x: (c.width / 2) - 174 / 2,
+        y: 50,
+        desenha() {
+          ctx.drawImage(
+            img,
+            mensagemGetReady.sX, mensagemGetReady.sY, //SpriteX, SpriteY
+            mensagemGetReady.w, mensagemGetReady.h, // Tamanho do recorte na sprite x e y
+            mensagemGetReady.x, mensagemGetReady.y,
+            mensagemGetReady.w, mensagemGetReady.h
+          )
+        }
+      }
+
+      //
+      // [Telas]
+      //
+      let telaAtiva = {}
+      function mudaParaTela(novaTela) {
+        telaAtiva = novaTela
+      }
+      const telas = {
+        INICIO: {
+          desenha() {
+            planoDeFundo.desenha()
+            chao.desenha()
+            flappyBird.desenha()
+            mensagemGetReady.desenha()
+          },
+          atualiza() {
+
+          },
+          click() {
+            mudaParaTela(telas.JOGO)
+          }
+        },
+
+        JOGO: {
+          desenha() {
+            planoDeFundo.desenha()
+            chao.desenha()
+            flappyBird.desenha()
+          },
+          atualiza() {
+            flappyBird.atualiza()
+          }
+        }
+      }
+
       function loop() {
-        planoDeFundo.desenha()
-        chao.desenha()
-
-        flappyBird.atualiza()
-        flappyBird.desenha()
-
+        telaAtiva.atualiza()
+        telaAtiva.desenha()
         requestAnimationFrame(loop)
       }
+
+      window.addEventListener('click', () => {
+        if (telaAtiva.click) {
+          telaAtiva.click()
+        }
+      })
+
+      mudaParaTela(telas.INICIO)
       loop()
     }
   }
